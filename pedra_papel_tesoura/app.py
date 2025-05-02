15457ffe79669b24cec3cbadb4b7a4ecc28801db
Fernando Henrique <fernandohsilva@outlook.com.br>
2025-05-02 00:44:59 -0300

    Jogo Pedra, Papel, Tesoura: Desafie o computador neste clássico jogo.
    Um jogo interativo onde você pode jogar Pedra, Papel, Tesoura contra o computador. Divertido e educativo!

---
+++ pedra_papel_tesoura/app.py
@@ -0,0 +1,48 @@
+import random
+
+user_points = 0
+computer_points = 0
+
+options = ["Pedra", "Papel", "Tesoura"]
+
+while True:
+    user_choice = input("Escolha 0(pedra), 1(papel) ou 2(tesoura) ou 'sair' para encerrar: ").strip()
+    
+    if user_choice in ["0", "1", "2"]:
+        user_choice = int(user_choice)
+        computer_choice = random.randint(0, 2)
+        computer_option = options[computer_choice]
+        
+        if user_choice == computer_choice:
+            print(f"Empate! Você e o computador escolheram {computer_option}.\n"
+                  "Placar atual:\n"
+                  f"Você: {user_points} pontos\n"
+                  f"Computador: {computer_points} pontos")
+        elif (user_choice == 0 and computer_choice == 2) or (user_choice == 1 and computer_choice == 0) or (user_choice == 2 and computer_choice == 1):
+            user_points += 1
+            print(f"Você venceu! Você escolheu {options[user_choice]} e o computador escolheu {computer_option}.\n"
+                  "Placar atual:\n"
+                  f"Você: {user_points} pontos\n"
+                  f"Computador: {computer_points} pontos")
+        else:
+            computer_points += 1
+            print(f"Computador venceu! Você escolheu {options[user_choice]} e o computador escolheu {computer_option}.\n"
+                  "Placar atual:\n"
+                  f"Você: {user_points} pontos\n"
+                  f"Computador: {computer_points} pontos")
+        continue
+    elif user_choice == 'sair':
+        print(f"Você saiu do jogo. O resultado final é:\n"
+              f"Você: {user_points} pontos\n"
+              f"Computador: {computer_points} pontos")
+       
+        if user_points > computer_points:
+            print("Você venceu!")
+        elif user_points < computer_points:
+            print("Computador venceu!")
+        else:
+            print("Empate!")
+        break
+    else: 
+        print("Opção inválida! Por favor, escolha 0(pedra), 1(papel) ou 2(tesoura).")
+        continue
